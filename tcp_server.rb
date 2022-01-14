c58c93378f1e919cdcc19440ea542c3057d4333d
Nedko Hristov <nedko@Nedkos-MacBook-Air.local>
2022-01-14 21:57:51 +0200

    Print port number and remove timestamp from the response

--- tcp_server.rb
+++ tcp_server.rb
@@ -2,16 +2,15 @@
 
 require 'socket'
 
-
-app = Proc.new do
-
-  ['200', {'Content-Type' => 'text/html'}, ["Hello world! The time is #{Time.now}"]]
-
+app = proc do
+  ['200', { 'Content-Type' => 'text/html' }, ["Hello world."]]
 end
-
 
 server = TCPServer.new 80
 
+port = server.addr[1]
+
+puts "Using port #{port}"
 
 while session = server.accept
 
@@ -19,37 +18,28 @@ while session = server.accept
 
   puts request
 
-
   # 1
 
   status, headers, body = app.call({})
-
 
   # 2
 
   session.print "HTTP/1.1 #{status}\r\n"
 
-
   # 3
 
   headers.each do |key, value|
-
     session.print "#{key}: #{value}\r\n"
-
   end
-
 
   # 4
 
   session.print "\r\n"
 
-
   # 5
 
   body.each do |part|
-
     session.print part
-
   end
 
   session.close
