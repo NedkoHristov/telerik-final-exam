4bc7c0e5f62fcf73916eccbdb435e0f4b47fae0b
Nedko Hristov <NedkoHristov@users.noreply.github.com>
2022-01-21 01:02:52 +0200

    GitHub CI integration (#2)
    Starting with the main objectives of the task, will be updated with enhancing the scope of the project
    
    * ruby http server - initial commit
    
    * Print port number and remove timestamp from the response
    
    * Initial CI setup, start with rubocop
    
    * Fix indentation
    
    * Fix typo
    
    * Fix version numbers
    
    * Define ruby version
    
    * Fix mistaken step name
    
    * Add gemfile requiring rubocop
    
    * Add bunch of linters
    
    * Debug why github_token is not recognized
    
    * Let's trigger GA ono push, not on pull request
    
    * Fail fast, change the rubocop action provider
    
    * Simplifying rubicop invocation
    
    * Remove duplicated items
    
    * Add Gemfile and secret to connect to GitHub API
    
    * Okay, let's start from the beginning
    
    * Fix rubocop offence, leave the annotations only
    
    * Rename CI file to something more meaningful
    
    * Intruduce breakman
    
    * Fix brakeman step
    
    * Fix brakeman step
    
    * Add the entire army
    
    * Fix rubocop spell
    
    * Add env variable for rubocop
    
    * Add code smell to demo the GH action reek
    
    * Update linters description
    
    * Add SNYK support
    
    * fix rubocop GH action
    
    * Fix GH Action - rubocop
    
    * Update GH rubocop action
    
    Fast feedback loops, remember!
    
    * Super fast feedback loop
    
    Disable rubocop for now, have to fix the repo structure and will fix this then.

--- tcp_server.rb
+++ tcp_server.rb
@@ -45,3 +45,11 @@ while (session = server.accept)
   session.close
 
 end
+
+# Smelly class
+class Smelly
+  # This will reek of UncommunicativeMethodName
+  def x
+    y = 10 # This will reek of UncommunicativeVariableName
+  end
+end
